5aa7d68ce0138388c3c5c086866c75e840a42821
MelanieSyu <mmilana.edu@gmail.com>
2019-12-08 19:42:55 +0400

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,168 @@
 class Convert {
-    convertToString = num => {
-        const numStrings = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
-        let word = '';
-    
-        if(!num) {
-            throw new Error('Введите число');
-        }
-        for(let i = 0; i < numStrings.length; i++) {
-            if(num === i) {
-                word = numStrings[i];
-            }
-        }
+    constructor(num) {
         
-        return word;
-    };
-}
+        numberToText = (num) => {
+            //вызов идет от большего числа к меньшему, удаляя считанное число тоесть: 154 читает 100 потом 50 потом 4 
+            if (!num || num === 0) {
+                throw new Error('Введите число больше нуля');
+            }
+        
+            let arrFrom0to10 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+            let arrFrom11to20 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+            let arrFrom21to99 = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
+            let str000 = ['hundred', 'thousand'];
+            let text = '';
+        
+            this.search(num);
+        
+            search = (num) => {  
+                num = `${num}`;
+                if (num != 0){
+                    switch (num.length) {
+                        case 1:
+                            this.from0to10(num);
+            
+                            break;
+                        case 2:
+                            this.from11to99(num);
+                        
+                            break;
+                        case 3:
+                            this.hundred(num);
+            
+                            break;
+                        case 4:
+                            this.thousand(num);
+            
+                            break;                 
+                        default:
+                            break;
+                    }
+                }
+            }  
+              
+            from0to10 = (num) => {
+        
+                text += arrFrom0to10[+num - 1];
+            }
+        
+            from11to99 = (num) => {
+                let firstNumber = (Math.trunc((+num)/10));
+                let secondNumber = ((+num)%10);
+                //let text;
+        
+                if (+num < 20) {
+                    text += arrFrom11to20[(secondNumber)]; 
+                } else {                 //если число больше 20 считать начало + вызов функции серч со 2 цифрой 
+                    text += arrFrom21to99[firstNumber-2] + ' ';
+        
+                    this.search(secondNumber);
+                }    
+            }
+        
+            hundred = (num) => {
+                let first = +num[0];
+                text += arrFrom0to10[first - 1] + ' ' + str000[0] + ' ';
+        
+                 //первые 
+        
+                this.search(num % 100); //идет вызывать ост часть
+            }
+        
+            thousand = (num) => {
+                let first = +num[0];
+                text += arrFrom0to10[first - 1] + ' ' + str000[1] + ' ' ;
+        
+                 //первые 
+        
+                this.search(num % 1000); //идет вызывать ост часть
+            }
+        
+            return text;
+        }
+    }
 
+     
+
+    // textToNumber = (text) => {
+    //     //вызов идет от большего числа к меньшему, удаляя считанное число тоесть: 154 читает 100 потом 50 потом 4 
+    //     if (!num || num === 0) {
+    //         throw new Error('Введите число больше нуля');
+    //     }
+    
+    //     let arrFrom0to10 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    //     let arrFrom11to20 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    //     let arrFrom21to99 = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
+    //     let str000 = ['hundred', 'thousand'];
+    //     let text = '';
+    
+    //     search(num);
+    
+    //     search = (num) => {  
+    //         num = `${num}`;
+    //         if (num != 0){
+    //             switch (num.length) {
+    //                 case 1:
+    //                     from0to10(num);
+        
+    //                     break;
+    //                 case 2:
+    //                     from11to99(num);
+                    
+    //                     break;
+    //                 case 3:
+    //                     hundred(num);
+        
+    //                     break;
+    //                 case 4:
+    //                     thousand(num);
+        
+    //                     break;                 
+    //                 default:
+    //                     break;
+    //             }
+    //         }
+    //     }  
+          
+    //     from0to10 = (num) => {
+    
+    //         text += arrFrom0to10[+num - 1];
+    //     }
+    
+    //     from11to99 = (num) => {
+    //         let firstNumber = (Math.trunc((+num)/10));
+    //         let secondNumber = ((+num)%10);
+    //         //let text;
+    
+    //         if (+num < 20) {
+    //             text += arrFrom11to20[(secondNumber)]; 
+    //         } else {                 //если число больше 20 считать начало + вызов функции серч со 2 цифрой 
+    //             text += arrFrom21to99[firstNumber-2] + ' ';
+    
+    //             search(secondNumber);
+    //         }    
+    //     }
+    
+    //     hundred = (num) => {
+    //         let first = +num[0];
+    //         text += arrFrom0to10[first - 1] + ' ' + str000[0] + ' ';
+    
+    //          //первые 
+    
+    //         search(num % 100); //идет вызывать ост часть
+    //     }
+    
+    //     thousand = (num) => {
+    //         let first = +num[0];
+    //         text += arrFrom0to10[first - 1] + ' ' + str000[1] + ' ' ;
+    
+    //          //первые 
+    
+    //         search(num % 1000); //идет вызывать ост часть
+    //     }
+    
+    
+    //     return text;
+    // }
+}
 export default Convert; 
--- test/index.test.js
+++ test/index.test.js
@@ -8,11 +8,11 @@ describe('Convert instance', () => {
     });
 });
 
-describe('Convert number in string', () => {
+describe('Convert number into string', () => {
     it('Should be show Error', () => {
         const convert = new Convert();
 
-        assert.throws(convert.convertToString, 'Введите число');
+        assert.throws(convert.numberToText, 'Введите число');
     });
     
     it('Should be convert 1 in string (one)', () => {
@@ -20,18 +20,87 @@ describe('Convert number in string', () => {
         const num = 1;
         const expected = 'one';
 
-        const actual = convert.convertToString(num);
+        const actual = convert.numberToText(num);
 
         assert.deepEqual(actual, expected);
     });
     
-    it('Should be convert 10 in string (ten)', () => {
+    it('Should be convert 20 in string (twenty)', () => {
         const convert = new Convert();
-        const num = 10;
-        const expected = 'ten';
+        const num = 20;
+        const expected = 'twenty ';
 
-        const actual = convert.convertToString(num);
+        const actual = convert.numberToText(num);
+
+        assert.deepEqual(actual, expected);
+    });
+        
+    it('Should be convert 3000 in string (thirty thousand)', () => {
+        const convert = new Convert();
+        const num = 3000;
+        const expected = 'three thousand ';
+
+        const actual = convert.numberToText(num);
+
+        assert.deepEqual(actual, expected);
+    });
+            
+    it('Should be convert 356 in string (thirty hundred fifty six)', () => {
+        const convert = new Convert();
+        const num = 356;
+        const expected = 'three hundred fifty six';
+
+        const actual = convert.numberToText(num);
 
         assert.deepEqual(actual, expected);
     });
 });
+
+
+// describe('Convert string into number', () => {
+//     it('Should be show Error', () => {
+//         const convert = new Convert();
+
+//         assert.throws(convert.numberToText, 'Введите число');
+//     });
+    
+//     it('Should be convert one into number (1)', () => {
+//         const convert = new Convert();
+//         const text = 'one';
+//         const expected = 1;
+
+//         const actual = convert.textToNumber(text);
+
+//         assert.deepEqual(actual, expected);
+//     });
+    
+//     it('Should be convert twenty into number (20)', () => {
+//         const convert = new Convert();
+//         const text = 'twenty ';
+//         const expected = 20;
+
+//         const actual = convert.textToNumber(text);
+
+//         assert.deepEqual(actual, expected);
+//     });
+        
+//     it('Should be convert three thousand into number (3000)', () => {
+//         const convert = new Convert();
+//         const text = 'three thousand ';
+//         const expected = 3000;
+
+//         const actual = convert.textToNumber(text);
+
+//         assert.deepEqual(actual, expected);
+//     });
+            
+//     it('Should be convert three hundred fifty six into number (356)', () => {
+//         const convert = new Convert();
+//         const text = 'three hundred fifty six';
+//         const expected = 356;
+
+//         const actual = convert.textToNumber(text);
+
+//         assert.deepEqual(actual, expected);
+//     });
+// });
